1282e1ecd4fcd4c1f7fe1f0103652598ba1de5b3
Jamie Lennox <jamielennox@gmail.com>
2016-02-15 19:00:30 -0600

    Reference the hosts primary_interface
    When you have multiple machines with different interfaces you can't
    assume that the primary_interface is the same everywhere. The default
    undercloud_floating_ip uses the current hosts primary_interface value to
    lookup something on another host. This fails when these are different so
    use the primary_interface specific to that host when looking these up.

--- roles/horizon/templates/etc/openstack-dashboard/local_settings.py
+++ roles/horizon/templates/etc/openstack-dashboard/local_settings.py
@@ -137,9 +137,9 @@ SECRET_KEY = "{{ secrets.horizon_secret_key }}"
 {% macro memcached_hosts() -%}
 {% for host in groups['controller'] -%}
    {% if loop.last -%}
-'{{ hostvars[host][primary_interface]['ipv4']['address'] }}:{{ memcached.port }}'
+'{{ hostvars[host][hostvars[host]['primary_interface']]['ipv4']['address'] }}:{{ memcached.port }}'
    {%- else -%}
-'{{ hostvars[host][primary_interface]['ipv4']['address'] }}:{{ memcached.port }}',
+'{{ hostvars[host][hostvars[host]['primary_interface']]['ipv4']['address'] }}:{{ memcached.port }}',
    {%- endif -%}
 {% endfor -%}
 {% endmacro -%}
